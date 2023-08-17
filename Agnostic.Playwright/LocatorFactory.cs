725b1f529ebd48f7c450fb9f4786a6841b25a949
Razvan Paraschivescu <razvan.paraschivescu@ubisoft.com>
2023-08-17 14:53:07 +0300

    adapter method from unsupported playwright types (id, name) to their css/xpath versions

---
+++ Agnostic.Playwright/LocatorFactory.cs
@@ -0,0 +1,23 @@
+﻿using Agnostic.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agnostic.Playwright
+{
+    internal static class LocatorFactory
+    {
+        internal static string GetSelector(this Locator locator)
+        {
+            return locator.Type switch
+            {
+                LocatorType.Id => $"#{locator.Value}",
+                LocatorType.Name => $"//*[@name='{locator.Value}']",
+                LocatorType.CssSelector or LocatorType.Xpath => locator.Value,
+                _ => throw new NotSupportedException($"Locator type '{locator.Type}' is not supported!")
+            };
+        }
+    }
+}
