4d64e1ccebd23c1ddb3f8779e56ecad8b566a5a2
Razvan Paraschivescu <razvan.paraschivescu@ubisoft.com>
2023-08-17 14:55:02 +0300

    tests are now running both selenium and playwright when triggered added more validation and tests

---
+++ Agnostic.Project.Tests/Tests.cs
@@ -0,0 +1,91 @@
+using Agnostic.Framework;
+using Microsoft.Playwright;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using Agnostic.Selenium;
+
+namespace Agnostic.Project.Tests
+{
+    public class Tests
+    {
+        IDriver _driver;
+
+        static IEnumerable<ITestCaseData> Strategies => new List<ITestCaseData> {
+            new TestCaseData(typeof(Agnostic.Selenium.Selenium)),
+            new TestCaseData(typeof(Agnostic.Playwright.Playwright))
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+            string type = TestContext.CurrentContext.Test.Arguments[0].ToString();
+            string seleniumType = typeof(Agnostic.Selenium.Selenium).ToString();
+            string playwrightType = typeof(Agnostic.Playwright.Playwright).ToString();
+
+            if (seleniumType == type) _driver = new Selenium.Selenium(new ChromeDriver());
+            else if (playwrightType == type) _driver = new Playwright.Playwright(Microsoft.Playwright.Playwright.CreateAsync().Result.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            }).Result);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+
+        [TestCaseSource(nameof(Strategies))]
+        public void TextInputFieldAcceptsInput(Type type)
+        {
+            var text = "Hello World";
+            var pom = new PomClass(_driver);
+
+            pom.GoToPage();
+            pom.EnterInputFieldText("Hello World");
+            var fieldText = pom.GetInputFieldText();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(fieldText, Is.EqualTo(text));
+            });
+        }
+
+        [TestCaseSource(nameof(Strategies))]
+        public void SelectDropdownCanSelect(Type type)
+        {
+            var text = "Set to 50%";
+            var metterValueExpected = "0.5";
+            var pom = new PomClass(_driver);
+
+            pom.GoToPage();
+            pom.SelectDropdownValue("50%");
+            var fieldText = pom.GetSelectedText();
+            var meterValue = pom.GetMetterValue();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(fieldText, Contains.Substring(text), $"Text '{text}' was not contained by element text '{fieldText}'");
+                Assert.That(meterValue, Is.EqualTo(metterValueExpected));
+            });
+        }
+
+        [TestCaseSource(nameof(Strategies))]
+        public void ButtonColorChangesOnClick(Type type)
+        {
+            var colorBefore = "green";
+            var colorAfter = "purple";
+
+            var pom = new PomClass(_driver);
+            pom.GoToPage();
+
+            Assert.That(pom.GetButtonColor(), Contains.Substring(colorBefore));
+
+            pom.ClickButton();
+
+            Assert.That(pom.GetButtonColor(), Contains.Substring(colorAfter));
+        }
+    }
+}
