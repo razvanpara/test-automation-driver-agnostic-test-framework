0f86efa2b5ce2145dd64b003c685807fa3cb6e65
Razvan Paraschivescu <razvan.paraschivescu@ubisoft.com>
2023-08-17 14:52:24 +0300

    updated the supported actions

--- Agnostic.Playwright/Playwright.cs
+++ Agnostic.Playwright/Playwright.cs
@@ -14,19 +14,29 @@ namespace Agnostic.Playwright
             _browser = browser;
             _page = browser.NewPageAsync().Result;
         }
-        public void Click(Locator element) => _page.ClickAsync(element.Value);
+        public void Click(Locator element)
+        {
+            _page.ClickAsync(element.GetSelector());
+            this.WaitFor(null, TimeSpan.FromMilliseconds(100));
+        }
 
         public void Dispose() => _browser.CloseAsync().Wait();
 
-        public void DragAndDrop(Locator element, Locator target) => _page.DragAndDropAsync(element.Value, target.Value);
+        public void DragAndDrop(Locator element, Locator target) => _page.DragAndDropAsync(element.GetSelector(), target.Value);
 
-        public void EnterText(Locator element, string text) => _page.Locator(element.Value).TypeAsync(text).Wait();
+        public void EnterText(Locator element, string text) => _page.Locator(element.GetSelector()).TypeAsync(text).Wait();
 
-        public string GetAttribute(Locator element, string attribute) => _page.InputValueAsync(element.Value).Result;
+        public string GetAttribute(Locator element, string attribute)
+        {
+            var selector = element.GetSelector();
+            var xpathExpr = $"() => document.evaluate(`{selector}`, document).iterateNext().{attribute};";
+            var other = $"() => document.querySelector('{selector}').{attribute}";
+            return _page.EvaluateAsync<string>(element.Type == LocatorType.Xpath ? xpathExpr : other).Result;
+        }
 
-        public string GetSelectedText(Locator element) => _page.InputValueAsync(element.Value).Result;
+        public string GetSelectedText(Locator element) => _page.EvaluateAsync<string>("(selector) => document.querySelector(selector).selectedOptions[0].innerText", element.GetSelector()).Result;
 
-        public string GetText(Locator element) => _page.Locator(element.Value).TextContentAsync().Result;
+        public string GetText(Locator element) => _page.Locator(element.GetSelector()).TextContentAsync().Result;
 
         public string GetUrl() => _page.Url;
 
@@ -34,32 +44,32 @@ namespace Agnostic.Playwright
 
         public void SelectIndex(Locator element, int index)
         {
-            _page.Locator(element.Value).SelectOptionAsync(new[]
+            _page.Locator(element.GetSelector()).SelectOptionAsync(new[]
         {
             new SelectOptionValue()
             {
                 Index = index,
             }
         });
-            Thread.Sleep(100);
+            this.WaitFor(null, TimeSpan.FromMilliseconds(100));
         }
 
 
         public void SelectValue(Locator element, string value)
         {
-            _page.Locator(element.Value).SelectOptionAsync(new[]
+            _page.Locator(element.GetSelector()).SelectOptionAsync(new[]
         {
             new SelectOptionValue()
             {
                 Value = value,
             }
         });
-            Thread.Sleep(100);
+            this.WaitFor(null, TimeSpan.FromMilliseconds(100));
         }
 
         public void WaitFor(Locator element, TimeSpan timeout)
         {
-            //throw new NotImplementedException();
+            Thread.Sleep(timeout);
         }
     }
 }
